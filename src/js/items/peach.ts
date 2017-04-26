9525a3aec2378216e3ce6ec3c7f5864c2975e8ce
Etienne Gauvin <etienne@gauvin.info>
2017-04-26 16:14:03 +0200

    Ajout d'une interface d'item

--- src/js/items/peach.ts
+++ src/js/items/peach.ts
@@ -1,12 +1,13 @@
-import Item from '../item'
 import game from '../game'
+import Item from '../item'
+import Eatable from './interfaces/eatable'
 
-export default class Peach extends Item {
+export default class Peach extends Item implements Eatable {
 
 	name = 'Peach'
 
 	geometry = game.data.models.peach.geometry
-	
+
 	materials = game.data.models.peach.materials
 
 }
